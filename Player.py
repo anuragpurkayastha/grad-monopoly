3e324354b2fcc35f0496d5efc04106bfe61d30be
Anurag <anuragpurkayastha@gmail.com>
2021-09-28 12:42:04 +1000

    Implemented a comparison method to compare two Player objects

--- Player.py
+++ Player.py
@@ -27,6 +27,9 @@ class Player:
         if ( amt >= 0 ):
             self.totalMoney -= amt
 
+    def setMoney(self, amt):
+        self.totalMoney = amt
+
     def isBankrupt(self):
         return self.totalMoney <= 0
 
@@ -36,5 +39,8 @@ class Player:
     def setCurrPos(self, index):
         self.currentPos = index
 
-    def toString(self):
-        return "Name:\t\t" + self.name + "\nTotal Money:\t$" + str(self.totalMoney) + "\nCurrent Position:\t\t" + str(self.currentPos)
+    def __repr__(self):
+        return "Name: " + self.name + ", Total Money: $" + str(self.totalMoney) + ", Current Position: " + str(self.currentPos)
+
+    def __eq__(self, other):
+        return self.name == other.name and self.totalMoney == other.totalMoney and self.currentPos == other.currentPos
