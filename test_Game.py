e6260b2ffccbed4cad44408b9d967dd17fb55caa
Anurag <anuragpurkayastha@gmail.com>
2021-09-28 12:43:34 +1000

    Started on tests for the Game class. This needs some work

---
+++ test_Game.py
@@ -0,0 +1,34 @@
+import unittest
+from Game import Game
+from Square import Square
+from Player import Player
+
+class TestGameMethod(unittest.TestCase):
+
+    maxDiff = None
+    def setUp(self):
+        self.board = [Square(name = "GO", sqr_type = "go"),
+                        Square(name = "The Burvale", price = 1, colour = "Brown", sqr_type = "property"),
+                        Square(name = "Fast Kebabs", price = 1, colour = "Brown", sqr_type = "property"),
+                        Square(name = "The Grand Tofu", price = 2, colour = "Red", sqr_type = "property"),
+                        Square(name = "Lanzhou Beef Noodle", price = 2, colour = "Red", sqr_type = "property"),
+                        Square(name = "Betty's Burgers", price = 3, colour = "Green", sqr_type = "property"),
+                        Square(name = "YOMG", price = 3, colour = "Green", sqr_type = "property"),
+                        Square(name = "Gami Chicken", price = 4, colour = "Blue", sqr_type = "property"),
+                        Square(name = "Massizim", price = 4, colour = "Blue", sqr_type = "property")
+                    ]
+
+        self.game = Game()
+
+    def test_get_board(self):
+        self.game.createBoard()
+        self.assertCountEqual(self.game.getBoard(), self.board)
+
+    def test_get_players(self):
+        self.game.createPlayers()
+        expected = [Player(name = "Peter"), Player(name = "Billy"), Player(name = "Charlotte"), Player(name = "Sweedal")]
+
+        self.assertCountEqual(self.game.getPlayers(), expected)
+
+if __name__ == '__main__':
+    unittest.main()
